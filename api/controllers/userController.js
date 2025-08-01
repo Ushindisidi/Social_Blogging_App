6f5025c20b653db1e9b7378ff59fba12f3e5a27f
Bereket-Eshete <bereketeshete63@gmail.com>
2025-08-01 08:45:48 +0300

    add res.status(500) for all controllers

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -8,7 +8,9 @@ export const getUser = async (req, res) => {
     const { password, ...others } = user._doc;
     res.status(200).json(others);
   } catch (err) {
-    res.status(500).json(err);
+    res
+      .status(500)
+      .json({ message: "Internal server error. Please try again." });
   }
 };
 export const deleteUser = async (req, res) => {
@@ -20,7 +22,9 @@ export const deleteUser = async (req, res) => {
         await User.findByIdAndDelete(req.params.id);
         res.status(200).json("User has been deleted...");
       } catch (err) {
-        res.status(500).json(err);
+        res
+          .status(500)
+          .json({ message: "Internal server error. Please try again." });
       }
     } catch (err) {
       res.status(404).json("User not found!");
@@ -45,7 +49,9 @@ export const updateUser = async (req, res) => {
       );
       res.status(200).json(updatedUser);
     } catch (err) {
-      res.status(500).json(err);
+      res
+        .status(500)
+        .json({ message: "Internal server error. Please try again." });
     }
   } else {
     res.status(401).json("You can update only your account!");
