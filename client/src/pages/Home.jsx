3059b52103b19e4d68bba55a6c34f21e589f9f99
Bereket-Eshete <bereketeshete63@gmail.com>
2025-08-02 23:47:11 +0300

    feat:add create post form

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import BlogSection from "../components/BlogSection";
 import ChatbotSection from "../components/ChatbotSection";
+import CreatePostForm from "../components/CreatePostForm";
 
 export default function Home() {
   return (
